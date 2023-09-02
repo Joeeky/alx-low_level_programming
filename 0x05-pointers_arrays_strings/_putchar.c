6d92bba3958ecefc9bd97c8b2bc87d6c6b96bc26
Jonathan Lazarus Oghenero <ogheneroeky@gmail.com>
2023-09-02 04:47:35 -0700

    3-strspn

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,1 +1,11 @@
-int _putchar(char c);
+/**
+ * _putchar - write the character c to stdout
+ * @c The character to print
+ *
+ * Return: On success 1
+ * On error, -1 is returned, and error id set well
+ */
+int _putchar(char c)
+{
+     return (write(1, &c, 1));
+}
