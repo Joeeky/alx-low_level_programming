412c8e4e0a275f0d8d459fa562a5772c269fb454
Jonathan Lazarus Oghenero <ogheneroeky@gmail.com>
2023-09-07 11:22:30 -0700

    1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,17 +1,47 @@
 #include "main.h"
 
 /**
- * _strlen - copies string to allocated memory
- * @s: string to newly memory
- *
- * Return: to new memory
+ * string_nconcat - function to concatnate strings with n bytes
+ * @s1: destination for concatnation
+ * @s2: source of string
+ * @n: int type for size of byte
+ * Return: pointer to new memory allocated
  */
-int _strlen(char *s)
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i = 0;
+	int count, count1;
+	int sign = n;
+	char *ptr;
+	int len1, len2;
 
-	while (s[i] != '\0')
-		i++;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-	return (i);
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+		;
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+		;
+
+	if (sign >= len2)
+	{
+		sign = len2;
+		ptr = malloc(sizeof(char) * (len1 + len2 + 1));
+	}
+	else
+		ptr = malloc(sizeof(char) * (len1 + n + 1));
+	if (ptr == NULL)
+		return (NULL);
+	for (count = 0; count < len1; count++)
+	{
+		ptr[count] = s1[count];
+	}
+	for (count1 = 0; count1 < sign; count1++)
+	{
+		ptr[count++] = s2[count1];
+	}
+	ptr[count++] = '\0';
+	return (ptr);
 }
